c04bcfc1610b83df07e4b7276c8267539d3ca687
581416_mgmri <lmachado@mgmresorts.com>
2026-01-16 17:21:04 -0300

    fix(test): tests for store management (MINOR)

--- src/entities/movies/model/movies.test.ts
+++ src/entities/movies/model/movies.test.ts
@@ -1,8 +1,11 @@
 jest.mock("#/shared/api/api", () => ({
   __esModule: true,
   getMovies: jest.fn(),
+  getMovieDetails: jest.fn(),
+  getMovieVideos: jest.fn(),
 }));
 
+import { faker } from "@faker-js/faker";
 import { allSettled, fork } from "effector";
 
 import deepmerge from "deepmerge";
@@ -10,9 +13,17 @@ import deepmerge from "deepmerge";
 import { $movies, INITIAL_DATA } from "#/entities/movies/model/movies";
 import { MoviesStore } from "../types";
 import { generateRandomMoviesStore } from "../lib/movies-utils";
-import { getMovies } from "#/shared/api/api";
-import { requestMoviesFx } from "./effects";
-import { setMovies } from "./events";
+import { getMovies, getMovieDetails, getMovieVideos } from "#/shared/api/api";
+import {
+  fetchMovieVideosFx,
+  requestMovieDetailsFx,
+  requestMoviesFx,
+} from "./effects";
+import {
+  setMovies,
+  setSelectedMovieDetails,
+  setSelectedMovieVideos,
+} from "./events";
 
 describe("Movies Store", () => {
   const mockedMoviesStore: MoviesStore = generateRandomMoviesStore();
@@ -23,7 +34,7 @@ describe("Movies Store", () => {
     expect(state).toEqual(INITIAL_DATA);
   });
 
-  it("should update the movies store as expected", async () => {
+  it("should update the movies store as expected when requestMoviesFx is successful", async () => {
     const mockMoviesInstance = deepmerge(INITIAL_DATA, mockedMoviesStore);
     const returnValue = {
       results: mockMoviesInstance.movies,
@@ -84,6 +95,123 @@ describe("Movies Store", () => {
     const result = await allSettled(requestMoviesFx, {
       params: {
         url: `http://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PRIVATE_MOVIES_TOKEN}&page=1`,
+      },
+      scope,
+    });
+
+    expect(result.status).toBe("done");
+    expect(result.value).toHaveProperty("error");
+    expect(result.value).toBe(returnValue);
+  });
+
+  it("should update the movies store as expected when requestMovieDetailsFx is successful", async () => {
+    const mockMoviesInstance = deepmerge(INITIAL_DATA, mockedMoviesStore);
+    const returnValue = mockMoviesInstance.currentMovieDetails;
+
+    (getMovieDetails as jest.Mock).mockResolvedValue(returnValue);
+
+    const scope = fork();
+
+    // Trigger requestMovieDetailsFx directly to ensure the mock is used
+    const result = await allSettled(requestMovieDetailsFx, {
+      params: {
+        url: `https://api.themoviedb.org/3/movie/${returnValue?.id}?language=en-US&api_key=${process.env.NEXT_PRIVATE_MOVIES_TOKEN}`,
+      },
+      scope,
+    });
+
+    expect(result.status).toBe("done");
+    expect(result.value).not.toHaveProperty("error");
+
+    if (!("error" in result.value) && result.status === "done") {
+      expect(result.value).toHaveProperty("poster_path");
+      expect(result.value.poster_path).toBe(
+        mockMoviesInstance.currentMovieDetails?.poster_path,
+      );
+
+      await allSettled(setSelectedMovieDetails, {
+        params:
+          result.status === "done" && !("error" in result.value)
+            ? result.value
+            : INITIAL_DATA.currentMovieDetails,
+        scope,
+      });
+
+      const moviesState = scope.getState($movies);
+
+      expect(moviesState.currentMovieDetails?.id).toBe(
+        mockMoviesInstance.currentMovieDetails?.id,
+      );
+    }
+  });
+
+  it("should handle the error thrown by requestMovieDetailsFx", async () => {
+    const errorMessage = "Failed to load requestMovieDetailsFx";
+    const returnValue = { error: errorMessage };
+    (getMovieDetails as jest.Mock).mockResolvedValue(returnValue);
+
+    const scope = fork();
+
+    // Calls the provided unit within the current scope and wait for all triggered effects to complete.
+    const result = await allSettled(requestMovieDetailsFx, {
+      params: {
+        url: `https://api.themoviedb.org/3/movie/${faker.number.int()}?language=en-US&api_key=${process.env.NEXT_PRIVATE_MOVIES_TOKEN}`,
+      },
+      scope,
+    });
+
+    expect(result.status).toBe("done");
+    expect(result.value).toHaveProperty("error");
+    expect(result.value).toBe(returnValue);
+  });
+
+  it("should update the movies store as expected when fetchMovieVideosFx is successful", async () => {
+    const mockMoviesInstance = deepmerge(INITIAL_DATA, mockedMoviesStore);
+    const returnValue = mockMoviesInstance.currentMovieVideos;
+
+    (getMovieVideos as jest.Mock).mockResolvedValue(returnValue);
+
+    const scope = fork();
+
+    // Trigger requestMovieDetailsFx directly to ensure the mock is used
+    const result = await allSettled(fetchMovieVideosFx, {
+      params: {
+        url: `https://api.themoviedb.org/3/movie/${mockMoviesInstance.currentMovieDetails?.id}/videos?language=en-US&api_key=${process.env.NEXT_PRIVATE_MOVIES_TOKEN}`,
+      },
+      scope,
+    });
+
+    expect(result.status).toBe("done");
+    expect(result.value).not.toHaveProperty("error");
+
+    if (!("error" in result.value) && result.status === "done") {
+      await allSettled(setSelectedMovieVideos, {
+        params:
+          result.status === "done" && !("error" in result.value)
+            ? result.value
+            : INITIAL_DATA.currentMovieVideos,
+        scope,
+      });
+
+      const moviesState = scope.getState($movies);
+
+      expect(moviesState.currentMovieVideos.length).toBe(
+        mockMoviesInstance.currentMovieVideos?.length,
+      );
+    }
+  });
+
+  it("should handle the error thrown by fetchMovieVideosFx", async () => {
+    const errorMessage = "Failed to load fetchMovieVideosFx";
+    const returnValue = { error: errorMessage };
+    (getMovieVideos as jest.Mock).mockResolvedValue(returnValue);
+
+    const scope = fork();
+
+    // Calls the provided unit within the current scope and wait for all triggered effects to complete.
+    const result = await allSettled(fetchMovieVideosFx, {
+      params: {
+        url: `https://api.themoviedb.org/3/movie/${faker.number.int()}/videos?language=en-US&api_key=${process.env.NEXT_PRIVATE_MOVIES_TOKEN}`,
       },
       scope,
     });
